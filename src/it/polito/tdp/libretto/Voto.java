fbdc40f249845532cc583d7f13d16c56523cde99
Giovanni Cioffi <giovanni.cioffi@student.tut.fi>
2019-03-11 14:06:08 +0100

    Aggiunta classe Libretto.

--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -3,6 +3,30 @@ package it.polito.tdp.libretto;
 import java.time.LocalDate;
 
 public class Voto {
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	public Voto(int voto, String corso, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
 	private int voto;
 	private String corso;
 	private LocalDate data; // da libreria Java.time per lavorare sulle date
